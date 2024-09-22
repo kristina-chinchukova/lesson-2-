bbd0100b789220618fc0b18317db177ab583dafc
kristina-chinchukova <kristina.chinchukova@gmail.com>
2024-09-22 20:05:33 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+homework = 12
+total_hours = 1.5
+name = 'Python'
+time = total_hours / homework
+print(name, 'всего задач:',homework, ",","затрачено часов:", total_hours, ",", "среднее время выполнения",time ,"часа." )
